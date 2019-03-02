89b2c4a8e662f9a389a0057fe3d3a9b8faec1af0
Diaby Cheick Mahomed Lamine <diabycheick@live.fr>
2019-03-02 10:29:49 +0000

    Création de la classe utilisateur

---
+++ Oc-GestionUtilisateurs/Models/Utilisateur.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel;
+
+namespace Oc_GestionUtilisateurs.Models
+{
+    public class Utilisateur : INotifyPropertyChanged
+    {
+        private string nom;
+        public string Nom
+        {
+            get { return nom; }
+            set
+            {
+                if (value != nom)
+                {
+                    nom = value;
+                    if (PropertyChanged != null)
+                    {
+                        PropertyChanged(this, new PropertyChangedEventArgs("Nom"));
+                    }
+                }
+            }
+        }
+
+        private string prenom;
+
+        public string Prenom
+        {
+            get { return prenom; }
+            set
+            {
+                if (value != prenom)
+                {
+                    prenom = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Prenom"));
+                }
+            }
+        }
+
+        private int age;
+        public int Age
+        {
+            get { return age; }
+            set
+            {
+                if (value != age)
+                {
+                    age = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Age"));
+                }
+            }
+        }
+
+        private string genre;
+        public string Genre
+        {
+            get { return genre; }
+            set
+            {
+                if (value != genre)
+                {
+                    genre = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Genre"));
+                }
+            }
+        }
+
+        private string profession;
+        public string Profession
+        {
+            get { return profession; }
+            set
+            {
+                if (value != profession)
+                {
+                    profession = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Profession"));
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
