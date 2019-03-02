a0880e0006fa6264b1a95b8e2e83e9ff210320c1
Diaby Cheick Mahomed Lamine <diabycheick@live.fr>
2019-03-02 10:47:29 +0000

    Ajout de la classe RelayCommand

---
+++ Oc-GestionUtilisateurs/ViewModels/FicheUtilisateurViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.Serialization;
+using System.Windows.Input;
+using System.Collections.ObjectModel;
+
+namespace Oc_GestionUtilisateurs.ViewModels
+{
+    public class FicheUtilisateurViewModel
+    {
+    }
+}
