954ce790af2696ebebd989c106137e15e6ce7a52
Diaby Cheick Mahomed Lamine <diabycheick@live.fr>
2019-03-03 13:18:45 +0000

    Suppression de la classe adresse et déplacement des prop dans utilisateur

--- Oc-GestionUtilisateurs/Models/Utilisateur.cs
+++ Oc-GestionUtilisateurs/Models/Utilisateur.cs
@@ -75,16 +75,72 @@ namespace Oc_GestionUtilisateurs.Models
             }
         }
 
-        private Adresse adresse;
-        public Adresse Adresse
+        private string pays;
+        public string Pays
         {
-            get { return adresse; }
+            get { return pays; }
             set
             {
-                if (!value.Equals(adresse))
+                if (value != pays)
                 {
-                    adresse = value;
-                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Adresse"));
+                    pays = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Pays"));
+                }
+            }
+        }
+
+        private string ville;
+        public string Ville
+        {
+            get { return ville; }
+            set
+            {
+                if (value != ville)
+                {
+                    ville = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Ville"));
+                }
+            }
+        }
+
+        private int? codePostal;
+        public int? CodePostal
+        {
+            get { return codePostal; }
+            set
+            {
+                if (value != codePostal)
+                {
+                    codePostal = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("CodePostal"));
+                }
+            }
+        }
+
+        private string rue;
+        public string Rue
+        {
+            get { return rue; }
+            set
+            {
+                if (value != rue)
+                {
+                    rue = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Rue"));
+                }
+            }
+        }
+
+        private int? numero;
+        public int? Numero
+        {
+            get { return numero; }
+            set
+            {
+                if (value != numero)
+                {
+                    numero = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Numero"));
                 }
             }
         }
